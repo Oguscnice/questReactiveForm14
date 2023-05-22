2f26790d56a4874db2cccf32535b91d1bc7b033d
Oguscnice <guillaume_cometto@hotmail.fr>
2023-05-22 15:03:34 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'questReactiveForm14';
+  username = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
+  street = new FormControl('');
+  zip = new FormControl('');
+  city = new FormControl('');
+  submit: boolean = false;
+  newUser!: any;
+
+  inscription() {
+    this.submit = true;
+
+    this.newUser = {
+      username: this.username.value,
+      email: this.email.value,
+      password: this.password.value,
+      adress: {
+        street: this.street.value,
+        zip: this.zip.value,
+        city: this.city.value,
+      },
+    };
+  }
 }
